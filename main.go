da044554aab194731c43082965a636b229c2540c
WeiseGuy <info@weiseguy.com>
2024-02-10 09:13:38 -0600

    Added basic function and handlers

--- main.go
+++ main.go
@@ -5,22 +5,22 @@ import (
     "net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "This is the home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+    sum := AddValues(2, 2)
+    _, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+func AddValues(x, y int) int {
+    return x + y
+}
+
 func main() {
-    /* 
-    HandleFunc takes, 
-    - the path name (URL), 
-    - an inline function that contains
-      + the ResponseWriter which writes responses to the user
-      + and a pointer to the Request function with takes in the request the user's browser sends
-        processes the request to send back a response
-    */
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        n, err := fmt.Fprintf(w, "Hello, World!") //Fprintf returns and int and an error type
-        if err != nil { // if there is an error, print the error to console
-            fmt.Println(err)
-        }
-        fmt.Println(fmt.Sprintf("Bytes written: %d", n))
-    })
+    http.HandleFunc("/", Home)
+    http.HandleFunc("/about", About)
 
     _ = http.ListenAndServe(":8080", nil)
 }
