b24387a89f2af0d5fca43cf00fb6039d57ce9174
WeiseGuy <info@weiseguy.com>
2024-02-11 20:27:16 -0600

    basic error checking

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "errors"
     "net/http"
 )
 
@@ -20,9 +21,30 @@ func AddValues(x, y int) int {
     return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+    f, err := divideValues(100.0, 0.0)
+    if err != nil {
+        fmt.Fprintf(w, "Cannot divide by 0")
+        // This return halts the function, stopping it from running the print statement below
+        return
+    }
+
+    fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+    if y <= 0 {
+        err := errors.New("cannot divide by zero")
+        return 0, err
+    }
+    result := x / y
+    return result, nil
+}
+
 func main() {
     http.HandleFunc("/", Home)
     http.HandleFunc("/about", About)
+    http.HandleFunc("/divide", Divide)
 
     fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
